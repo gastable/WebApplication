d24de5df8f8aad04fc425c9a8345fca98fd365d8
gastable <Huang681228@gmail.com>
2022-12-05 10:35:20 +0800

    新增上傳多個檔案controller

---
+++ 01Controller/Controllers/MultiFileUploadController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01Controller.Controllers
+{
+    public class MultiFileUploadController : Controller
+    {
+       
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase[] photos)
+        {
+           if(photos != null)
+            {
+                foreach(var photo in photos)
+                {
+                    if(photo.ContentLength>0)
+                    {
+                        photo.SaveAs(Server.MapPath("~/photos/"+photo.FileName));
+                        ViewBag.Message = "上傳成功";
+                    }
+                   
+                }
+            }
+            else
+            {
+                ViewBag.Message = "您沒有上傳任何檔案!!";
+            }
+
+            return View();
+        }
+    }
+}
